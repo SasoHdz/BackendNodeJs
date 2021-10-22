82fa042e72ef715942ce6694c0e544f6ab12bdbf
= <=>
2021-10-21 23:52:14 -0500

    Creando mas rutas, jugando con res.json

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto1',
+    precio: 123
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
