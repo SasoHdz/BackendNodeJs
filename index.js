1e8ee044d61376d0c1fb68b9a252da2fd5276d95
= <=>
2021-10-23 00:19:45 -0500

    Separacion de responsabilidades

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
@@ -10,38 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto1',
-      precio: 1000
-    },
-    {
-      name: 'Producto2',
-      precio: 1200
-    }
-  ]);
-});
-
-app.get('/products/:id', (req,res) => {
-
-  const { id } = req.params;
-
-  res.json({
-    id,
-    name: 'Producto1',
-    precio: 123
-  });
-})
-
-app.get('/categories/:categoryId/products/:productsId',( req, res)=>{
-  const { categoryId, productsId } = req.params;
-  res.json({
-    categoryId,
-    productsId,
-  });
-
-})
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port' + port);
